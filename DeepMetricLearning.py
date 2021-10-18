315a6dd725f02a25e0bbaf2d2f8cf98e1f1efe3e
Kimiaki0619 <cski20014@g.nihon-u.ac.jp>
2021-10-18 11:29:11 +0900

    cnnとarcfaceの追加

--- DeepMetricLearning.py
+++ DeepMetricLearning.py
@@ -15,13 +15,13 @@ def prepare_full_MNIST_databunch(data_folder, tfms):
         img(class)_(count index).png
     """
     train_ds = datasets.MNIST(data_folder, train=True, download=True,
-                          transform=transforms.Compose([
-                              transforms.Normalize((0.1307,), (0.3081,))
-                          ]))
+                        transform=transforms.Compose([
+                            transforms.Normalize((0.1307,), (0.3081,))
+                        ]))
     valid_ds = datasets.MNIST(data_folder, train=False,
-                              transform=transforms.Compose([
-                                  transforms.Normalize((0.1307,), (0.3081,))
-                              ]))
+                            transform=transforms.Compose([
+                                transforms.Normalize((0.1307,), (0.3081,))
+                            ]))
 
     def have_already_been_done():
         return (data_folder/'images').is_dir()
@@ -82,3 +82,108 @@ def get_embeddings(embedding_model, data_loader, label_catcher=None, return_y=Fa
         return embs, ys
     return embs
 
+# T-sneで根拠を可視化
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from mpl_toolkits.mplot3d import axes3d
+#%matplotlib notebook
+
+
+def show_2D_tSNE(latent_vecs, target, title='t-SNE viz'):
+    latent_vecs = latent_vecs
+    latent_vecs_reduced = TSNE(n_components=2, random_state=0).fit_transform(latent_vecs)
+    plt.scatter(latent_vecs_reduced[:, 0], latent_vecs_reduced[:, 1],
+                c=target, cmap='jet')
+    plt.colorbar()
+    plt.show()
+
+
+def show_3D_tSNE(latent_vecs, target, title='3D t-SNE viz'):
+    latent_vecs = latent_vecs
+    tsne = TSNE(n_components=3, random_state=0).fit_transform(latent_vecs)
+    fig = plt.figure(figsize=(13,10))
+    ax = fig.add_subplot(111, projection='3d')
+    scatter = ax.scatter3D(tsne[:, 0], tsne[:, 1], tsne[:, 2], c=target, cmap='jet')
+    ax.set_title(title)
+    plt.colorbar(scatter)
+    plt.show()
+
+
+def show_as_PCA(latent_vecs, target, title='PCA viz'):
+    latent_vecs = latent_vecs
+    #latent_vecs_reduced = TSNE(n_components=2, random_state=0).fit_transform(latent_vecs)
+    latent_vecs_reduced = PCA(n_components=2).fit_transform(latent_vecs)
+    plt.scatter(latent_vecs_reduced[:, 0], latent_vecs_reduced[:, 1],
+                c=target, cmap='jet')
+    plt.colorbar()
+    plt.show()
+
+#dataを入れる。
+DATA = Path('data')
+data = prepare_full_MNIST_databunch(DATA, get_transforms(do_flip=False))
+
+#まず可視化、何も使わずに可視化を行なっている。
+raw_x = np.array([a.data.numpy() for a in data.valid_ds.x])
+raw_y = np.array([int(y.obj) for y in data.valid_ds.y])
+raw_x = raw_x.reshape((len(raw_x), -1))
+
+if False: # for saving time
+    LIMIT = 1000
+    chosen_idxes = np.random.choice(list(range(len(raw_x))), LIMIT)
+    raw_x = raw_x[chosen_idxes]
+    raw_y = raw_y[chosen_idxes]
+
+show_2D_tSNE(raw_x, raw_y, 'Raw sample distributions (t-SNE)')
+
+#cnn 普通のcnnでの学習。比較対象
+def learner_conventional(train_data):
+    learn = cnn_learner(train_data, models.resnet18, metrics=accuracy)
+    learn.fit(1)
+    learn.unfreeze()
+    learn.fit(3)
+    return learn
+
+learn = learner_conventional(data)
+embs = get_embeddings(body_feature_model(learn.model), data.valid_dl)
+show_2D_tSNE(embs, [int(y) for y in data.valid_ds.y], title='Simply trained　CNN (t-SNE)')
+
+#arcfaceでの学習
+from metrics import *
+
+class LabelCatcher(LearnerCallback):
+    last_labels = None
+
+    def __init__(self, learn:Learner):
+        super().__init__(learn)
+        
+    def on_batch_begin(self, last_input, last_target, train, **kwargs):
+        LabelCatcher.last_labels = last_target
+        return {'last_input': last_input, 'last_target': last_target} 
+
+
+class XFaceNet(nn.Module):
+    def __init__(self, org_model, data, xface_product=ArcMarginProduct, m=0.5):
+        super().__init__()
+        self.org_model = org_model
+        self.feature_model = body_feature_model(org_model)
+        self.metric_fc = xface_product(512, data.c, m=m).cuda()
+    
+    def forward(self, x):
+        x = self.feature_model(x)
+        x = self.metric_fc(x, LabelCatcher.last_labels)
+        return x
+
+
+def learner_ArcFace(train_data):
+    learn = cnn_learner(train_data, models.resnet18, metrics=accuracy)
+    learn.model = XFaceNet(learn.model, train_data, ArcMarginProduct, m=0.5)
+    learn.callback_fns.append(partial(LabelCatcher))
+    learn.fit(1)
+    learn.unfreeze()
+    learn.fit(5)
+    return learn
+
+learn = learner_ArcFace(data)
+embs = get_embeddings(body_feature_model(learn.model), data.valid_dl)
+show_2D_tSNE(embs, [int(y) for y in data.valid_ds.y], title='ArcFace (t-SNE)')
